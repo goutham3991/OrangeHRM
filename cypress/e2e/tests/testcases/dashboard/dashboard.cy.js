f9ebdf2c891649413170ec065cd9782cdf8a3bad
Goutham Madugula <goutham.madugula@abrigo.com>
2024-07-01 15:53:26 +0530

    Modified Framework

---
+++ cypress/e2e/tests/testcases/dashboard/dashboard.cy.js
@@ -0,0 +1,23 @@
+//<reference types=”cypress”/>
+import 'cypress-wait-until';
+import dashboard from '../../pages/dashboard';
+import loginPage from '../../pages/loginPage';
+
+describe('verify dashboard page',()=>{
+
+    it('check dahsboard page details',()=>{
+
+        let loginpage = new loginPage()
+        let dash = new dashboard()
+        let username = 'Admin'
+        let password = 'admin123'
+        let dashboardMenu = 'Dashboard'
+        let title = 'OrangeHRM'
+        
+        loginpage.getLogin(username,password)
+        dash.getDashboardPage()
+        dash.getDashboardMenus()
+        dash.goToDashboardMenu(dashboardMenu)
+        dash.getTitle(title)
+    })
+})
---
+++ cypress/e2e/tests/testcases/login/orangeHRM.cy.js
@@ -0,0 +1,15 @@
+import loginPage from '../../pages/loginPage';
+
+describe('Scenario: Check OrangeHRM login Functionality',()=>{
+    it('Test Case: Verify Successfull Login Functionality',()=>{
+
+        let loginpage = new loginPage()
+        
+        let username = 'Admin'
+        let password = 'admin123'
+        let pageTitle = 'OrangeHRM'
+        
+        loginpage.getLogin(username,password)
+        loginpage.getTitle(pageTitle)
+    })
+})
