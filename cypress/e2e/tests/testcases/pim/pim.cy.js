9b3da9c9695a980455672b9a32e4553ae44d6244
Goutham Madugula <goutham.madugula@abrigo.com>
2024-07-28 17:03:18 +0530

    All Test Cases

--- cypress/e2e/tests/testcases/pim/pim.cy.js
+++ cypress/e2e/tests/testcases/pim/pim.cy.js
@@ -2,9 +2,9 @@ import pimpage from "../../pages/PIM"
 
 describe('Scenario: Test PIM Page', () => {
     let pimpageObj = new pimpage()
-    let employeeName = pimpageObj.name()
-    let employeeMiddleName = pimpageObj.middleName()
-    let employeeLastName = pimpageObj.enterLastName()
+    //let employeeName = pimpageObj.name()
+    //let employeeMiddleName = pimpageObj.middleName()
+    //let employeeLastName = pimpageObj.enterLastName()
 
     it.skip('Test Case: Test PIM page functionlity', () => {
         cy.loginWithCred()
